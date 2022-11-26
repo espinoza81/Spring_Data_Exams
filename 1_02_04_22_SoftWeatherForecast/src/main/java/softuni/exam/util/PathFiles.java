36084e1b932dd72feb52ca5d309e056b51d9decd
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-11-26 19:08:46 +0200

    Delete 1_02_04_22_SoftWeatherForecast directory

--- 1_02_04_22_SoftWeatherForecast/src/main/java/softuni/exam/util/PathFiles.java
+++
@@ -1,9 +0,0 @@
-package softuni.exam.util;
-
-import java.nio.file.Path;
-
-public class PathFiles {
-    public static final Path CITIES_PATH = Path.of("src", "main", "resources", "files", "json", "cities.json");
-    public static final Path COUNTRIES_PATH = Path.of("src", "main", "resources", "files", "json", "countries.json");
-    public static final Path FORECASTS_PATH = Path.of("src", "main", "resources", "files", "xml", "forecasts.xml");
-}
